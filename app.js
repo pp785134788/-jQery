d774462ead9435a384632a0f16b35333cbf61c77
pp785134788 <54616108+pp785134788@users.noreply.github.com>
2019-10-09 17:20:00 +0800

    搭建服务器的基本模块

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // 负责开启服务器+注册中间件
+
+// 引入第三方 express 模块
+const express = require('express');
+//引入第三方 bodyParser 模块
+const bodyParser = require('body-parser');
+//引入 ./router/user 分支路由
+const userRouter = require('./router/user');
+//创建服务器框架
+const app = express();
+
+app.listen(8080,()=>{
+    console.log('http://127.0.0.1:8080');
+});
+
+//处理静态资源
+app.use('/assets',express.static('assets'));
+
+//注册body-parser的中间件
+app.use(bodyParser.urlencoded({extended:false}));
+
+//设置 ejs 为默认的模板引擎，但是这个模板引擎的目的不是为了做服务器渲染，而是为了返回页面更加方便
+app.set('view engine','ejs');
+
+
+//注册路由
+/*
+    分支路由：
+        注册的方式和以前的一个路由稍有不同
+        app.use('/以书面开头',router对象);
+ */
+app.use('/admin/user',userRouter);
--- router/user.js
+++ router/user.js
@@ -1,1 +1,14 @@
 // 负责跟用户操作相关的路由
+// 引入第三方 express 模块
+const express = require('express');
+//创建路由对象
+const router = express.Router();
+
+//监听 用户 登陆的页面请求
+router.get('/login.html',(req,res)=>{
+    //返回一个登陆用的页面
+    res.render('admin/login');
+});
+
+//暴露路由对象
+module.exports = router;
